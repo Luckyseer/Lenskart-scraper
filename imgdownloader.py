1b159821a7ceaff4fdbb9e20541d0e4d275f9971
Luckyseer <36114443+Luckyseer@users.noreply.github.com>
2020-03-26 22:00:53 +0530

    Updated image downloader
    Now creates a csv file with the image names in order of download in order to easily keep track of images

--- imgdownloader.py
+++ imgdownloader.py
@@ -3,14 +3,19 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time
 import requests
 import os
+import csv
 from tqdm import tqdm
+
+pause = False  # Set to false to create csv file
 
 firefox = webdriver.Firefox()
 num = 1
-with open('links.txt', 'r') as f:
+with open('links2.txt', 'r') as f:
     links = f.readlines()
     num_links = len(links)
     print("No. of links: %d" % num_links)
+
+image_list = []
 
 
 def download(url, pathname):
@@ -36,12 +41,11 @@ def download(url, pathname):
             progress.update(len(data))
 
 
-for i in range(0, num_links):
+for i in range(num - 1, num_links):
     firefox.get(links[i])
-
     images = firefox.find_elements_by_tag_name('img')
-    new_images = []
     imglen = 0
+    image_dict = {'num': i}  # dictionary to keep track of filenames in order of download
     for image in images:
 
         if 'thumbnail' in image.get_attribute('src') and imglen <= 6:
@@ -51,13 +55,25 @@ for i in range(0, num_links):
             img2 = firefox.find_elements_by_tag_name('img')
             for image2 in img2:
                 try:
-                    if image2 not in images and 'bat.bing' not in image2.get_attribute('src'):
-                            download(image2.get_attribute('src'), 'images')
+                    if imglen > 4:  # downloads only 5 images
+                        pass
+                    elif image2 not in images and 'bat.bing' not in image2.get_attribute('src'):
+                            download(image2.get_attribute('src'), 'sunglass_images')
+                            image_dict['img%d' % imglen] = image2.get_attribute('src').split("/")[-1]
                             imglen += 1
                 except:
-                        print('Exception at: %d'%num)
-        if imglen > 6:  # downloads only 6 images
-            pass
+                        print('Exception at: %d' % num)
+    image_list.append(image_dict)
     print('Completed (%d/%d)' % (num, num_links))
     num += 1
 firefox.close()
+filename = 'sunglass_image.csv'
+if not pause:
+    num2 = 1
+    with open(filename, 'w', newline='') as f:
+        w = csv.DictWriter(f, ['num', 'img0', 'img1', 'img2', 'img3', 'img4'])
+        w.writeheader()
+        for image_name in image_list:
+            w.writerow(image_name)
+            print('Entered (%d/%d) Entries' % (num2, num_links))
+            num2 += 1
